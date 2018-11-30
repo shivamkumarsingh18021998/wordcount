f81303631d299c17da9b5d99600e70c12eb113c6
shivam kumar singh <shivamkumarsingh18021998@gmail.com>
2018-12-01 03:37:08 +0530

    anothercommit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.homepage,name='home'),
+    path('',views.home,name='home'),
     path('count',views.count,name='count'),
 ]
